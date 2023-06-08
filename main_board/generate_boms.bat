60b1fe989126b8f875b3fc74bdeff01e57fad030
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-06-08 07:10:43 -0700

    Updates main_board, uses generate_boms.bat
    Updates reverse protection circuitry on main board.
    Updates 3.3V reference circuitry.
    Uses generate_boms and retires .xlsx BOM files.

---
+++ main_board/generate_boms.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo This creates BOM files for building the Bonkulator
+echo It requires main_board.xml as an input. Make sure that it is up to date. Use the KiCAD schematic BOM generator with the bom2grouped_csv.xsl plugin that's supplied with KiCAD.
+
+set bin_dir=C:\Program Files\KiCad\bin
+set plugin_dir=C:\Users\shann\Dropbox\KiCAD\BOM
+set base_file=main_board
+
+"%bin_dir%\python" "%plugin_dir%/bom_lcsc.py" "%base_file%.xml" "bom\LCSC\%base_file%_bom.csv"
+
+"%bin_dir%\python" "%plugin_dir%/bom_digikey_split.py" "%base_file%.xml" "bom\Digi-Key\%base_file%_split.csv"
+
+"%bin_dir%\python" "%plugin_dir%/bom_digikey_full.py" "%base_file%.xml" "bom\Digi-Key\%base_file%_full.csv"
+
+if not errorlevel 1 echo Success!
+
+pause
