1b38b128ff60f73a3b3df9fbee8707c7dcc88012
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-10-29 21:18:30 -0700

    Adds Bounce support
    Change name of output to channel for triggers.

--- code/Bonkulator/TRIGGER.cpp
+++ code/Bonkulator/TRIGGER.cpp
@@ -7,17 +7,21 @@
 
 extern String toJSON(String key, String value);
 extern void trigger_output(byte trig_num, int output_num);
+extern void trigger_bounce(byte trig_num, trigger_state input_num);
 extern bool in_output_fxn();
+extern bool in_bounce_fxn();
 extern void code_red(bool);
+extern class Greenface_gadget *bounce_inputs[];
+extern int bounce_debug;
 
 TRIGGER::TRIGGER(int _trig_num)
 {
     trig_num = _trig_num;
     blanking_period = 0;
-    for (int output_num = 0; output_num < NUM_OUTPUTS; output_num++)
+    for (int channel_num = 0; channel_num < TRIGGER_CTRL_CHANS; channel_num++)
     {
-        blanking_periods[output_num] = 0;
-        trig_ctrl_vals[output_num] = 0;
+        blanking_periods[channel_num] = 0;
+        trig_ctrl_vals[channel_num] = 0;
     }
 }
 
@@ -36,7 +40,7 @@ void TRIGGER::trigger()
         {
             uint16_t ctrl_val = trig_ctrl_vals[output_num];
             uint8_t trig_ctrl = trig_ctrls[output_num];
-            if (get_output(output_num))
+            if (get_channel(output_num))
             {
                 // trigger_output(trig_num, output_num); // for testing
                 switch (trig_ctrl)
@@ -68,6 +72,44 @@ void TRIGGER::trigger()
             }
         }
     }
+    else if (in_bounce_fxn())
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            // bounce_debug = (*bounce_inputs[i]).get_param(2 + trig_num);
+            uint8_t trig_ctrl = (*bounce_inputs[i]).get_param(11); // BOUNCE_TRIG_CTRL
+            uint8_t ctrl_val = (*bounce_inputs[i]).get_param(12);  // BOUNCE_TRIG_CTRL_VAL
+            if ((*bounce_inputs[i]).get_param(2 + trig_num))       // 2 + trig_num  2 = BOUNCE_ENABLE_T0
+            {
+                switch (trig_ctrl)
+                {
+                case TRIG_HOLDOFF:
+                    if (start_time > blanking_periods[i + 8])
+                    {
+                        trigger_bounce(trig_num, state);
+                        blanking_periods[i] = start_time + ctrl_val;
+                    }
+                    break;
+                case TRIG_SKIP:
+                    if (counts[i + 8]++ > ctrl_val)
+                    {
+                        trigger_bounce(trig_num, state);
+                        counts[i + 8] = 0;
+                    }
+                    break;
+                case TRIG_DENSITY:
+                    if (random(0, 100) > 100 - ctrl_val)
+                    {
+                        trigger_bounce(trig_num, state);
+                    }
+                    break;
+                default:
+                    code_red(true);
+                    // trigger_output(trig_num, output_num);
+                }
+            }
+        }
+    }
 }
 
 // used by user waveform recording
@@ -79,13 +121,13 @@ void TRIGGER::clear()
 
 void TRIGGER::disable_all()
 {
-    outputs = 0;
+    channels = 0;
     // Serial.println("Disabling triggers for TRIG: " + String(trig_num));
 }
 
 void TRIGGER::trigger_report()
 {
-    Serial.println("Trigger " + String(trig_num) + ": " + String(outputs));
+    Serial.println("Trigger " + String(trig_num) + ": " + String(channels));
 }
 
 String TRIGGER::params_toJSON()
@@ -96,41 +138,41 @@ String TRIGGER::params_toJSON()
     out += ", ";
     out += toJSON("state", String(state));
     out += ", ";
-    out += toJSON("outputs", String(outputs));
+    out += toJSON("outputs", String(channels));
     out += " }";
     return out;
 }
 
-void TRIGGER::set_output(int output_num, bool val)
+void TRIGGER::set_channel(int channel_num, bool val)
 {
     if (val)
     {
-        bitSet(outputs, output_num);
+        bitSet(channels, channel_num);
     }
     else
     {
-        bitClear(outputs, output_num);
+        bitClear(channels, channel_num);
     }
     if (state == TRIGGER_ACTIVE)
     {
-        trigger_output(trig_num, output_num);
+        trigger_output(trig_num, channel_num);
     }
-    if (outputs == 0)
+    if (channels == 0)
     {
         clear();
     }
 }
 
-void TRIGGER::set_trig_ctrls(int output_num, uint8_t trig_ctrl, uint16_t ctrl_val)
+void TRIGGER::set_trig_ctrls(int channel_num, uint8_t trig_ctrl, uint16_t ctrl_val)
 {
-    trig_ctrls[output_num] = trig_ctrl;
-    trig_ctrl_vals[output_num] = ctrl_val;
+    trig_ctrls[channel_num] = trig_ctrl;
+    trig_ctrl_vals[channel_num] = ctrl_val;
     start();
-    blanking_periods[output_num] = start_time + ctrl_val;
-    // Serial.println(" set_trig_ctrls: output - " + String(output_num) + " ctrl: " + String(trig_ctrl) + " val: " + String(ctrl_val));
+    blanking_periods[channel_num] = start_time + ctrl_val;
+    // Serial.println(" set_trig_ctrls: output - " + String(channel_num) + " ctrl: " + String(trig_ctrl) + " val: " + String(ctrl_val));
 }
 
-bool TRIGGER::get_output(int output_num)
+bool TRIGGER::get_channel(int channel_num)
 {
-    return bitRead(outputs, output_num) == 1;
+    return bitRead(channels, channel_num) == 1;
 }
--- code/Bonkulator/TRIGGER.h
+++ code/Bonkulator/TRIGGER.h
@@ -16,6 +16,8 @@ typedef enum _trigger_state
     TRIGGER_ACTIVE = 1,
 } trigger_state;
 
+#define TRIGGER_CTRL_CHANS 10
+
 class TRIGGER
 {
 public:
@@ -27,19 +29,19 @@ public:
     void disable_all();
     void trigger_report();
     // void do_cmd(char cmd);
-    void set_output(int output_num, bool val);
-    void set_trig_ctrls(int output_num, uint8_t trig_ctrl, uint16_t trig_ctrl_val);
-    bool get_output(int output_num);
+    void set_channel(int channel_num, bool val);
+    void set_trig_ctrls(int channel_num, uint8_t trig_ctrl, uint16_t trig_ctrl_val);
+    bool get_channel(int channel_num);
     String params_toJSON();
     String get_state();
     trigger_state state; // Only used by check_user_recording(). Todo: resolve this issue
 
 private:
-    uint32_t outputs;                  // up to 32 outputs. 8 for now
-    uint8_t trig_ctrls[8];             // 8 outputs
-    uint16_t trig_ctrl_vals[8];        // 8 outputs
-    unsigned long blanking_periods[8]; // 8 outputs
-    unsigned long counts[8];           // 8 outputs
+    uint32_t channels;                                  // up to 32 channels. 8 outputs, 2 Inputs
+    uint8_t trig_ctrls[TRIGGER_CTRL_CHANS];             // 8 outputs, 2 Inputs
+    uint16_t trig_ctrl_vals[TRIGGER_CTRL_CHANS];        // 8 outputs, 2 Inputs
+    unsigned long blanking_periods[TRIGGER_CTRL_CHANS]; // 8 outputs, 2 Inputs
+    unsigned long counts[TRIGGER_CTRL_CHANS];           // 8 outputs, 2 Inputs
     unsigned long start_time;
     long blanking_period = 0;
 };
