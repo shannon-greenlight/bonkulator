5f36bddb0d919be3293cc13a1df0fa7a0b03b4c2
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-10-31 07:40:12 -0700

    Work on Bounce
    Removes CV params.
    Makes scale and offset params use offsets. Now bipolar range.
    Moves calc code from Bonkulator.ino
    Calculates bounce correctly.

--- code/Bonkulator/bounce_fxns.h
+++ code/Bonkulator/bounce_fxns.h
@@ -8,8 +8,6 @@ enum
     BOUNCE_ENABLE_T1,
     BOUNCE_ENABLE_T2,
     BOUNCE_ENABLE_T3,
-    BOUNCE_CV0,
-    BOUNCE_CV1,
     BOUNCE_SCALE,
     BOUNCE_OFFSET,
     BOUNCE_SAMPLE_TIME,
@@ -18,23 +16,34 @@ enum
     BOUNCE_NUM_PARAMS,
 };
 
-uint16_t _bounce_mins[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, TRIG_HOLDOFF, 0};
-uint16_t _bounce_maxs[] = {7, 1, 1, 1, 1, 1, CV_TRIGGER, CV_TRIGGER, 100, 100, 32767, TRIG_DENSITY, MAX_MS};
-uint16_t _bounce_init_vals[] = {0, 0, 0, 0, 0, 0, 0, 0, 100, 0, 100, TRIG_HOLDOFF, 5};
-String bounce_string_params[] = {"", "No, Yes", TRIGGER_STATES, TRIGGER_STATES, TRIGGER_STATES, TRIGGER_STATES, CV_TYPES, CV_TYPES, "", "", "", "Hold_Off,Skip,Density", ""};
+#define OUTPUT_CV_TYPES "Off   ,Scale ,Offset,Sample Time,Trigger"
+
+uint16_t _bounce_mins[] = {0, 0, 0, 0, 0, 0, 0, 0, 10, TRIG_HOLDOFF, 0};
+uint16_t _bounce_maxs[] = {7, 1, 1, 1, 1, 1, 200, 200, 32767, TRIG_DENSITY, MAX_MS};
+uint16_t _bounce_init_vals[] = {0, 0, 0, 0, 0, 0, 200, 100, 100, TRIG_HOLDOFF, 5};
+String bounce_string_params[] = {"", "No, Yes", TRIGGER_STATES, TRIGGER_STATES, TRIGGER_STATES, TRIGGER_STATES, "", "", "", "Hold_Off,Skip,Density", ""};
 // int16_t bounce_offsets[] = {0, BOUNCE_OFFSET_CORRECTION, 0}; // allows negative numbers
 
 uint16_t _bounce0_params[BOUNCE_NUM_PARAMS];
 uint16_t *bounce0_stuff[] = {_bounce0_params, _bounce_mins, _bounce_maxs, _bounce_init_vals};
-String bounce0_labels[] = {"Output: ", "Repeat: ", "T0: ", "T1: ", "T2: ", "T3: ", "CV0: ", "CV1: ", "Scale: ", "Offset: ", "Sample Time: ", "Trig Crtl: ", "Trig Holdoff: "};
+String bounce0_labels[] = {"Output: ", "Repeat: ", "T0: ", "T1: ", "T2: ", "T3: ", "Scale: ", "Offset: ", "Sample Time: ", "Trig Crtl: ", "Trig Holdoff: "};
 Greenface_gadget bounce0_fxn("Bounce 0", bounce0_labels, bounce0_stuff, sizeof(_bounce0_params) / sizeof(_bounce0_params[0]));
 
 uint16_t _bounce1_params[BOUNCE_NUM_PARAMS];
 uint16_t *bounce1_stuff[] = {_bounce1_params, _bounce_mins, _bounce_maxs, _bounce_init_vals};
-String bounce1_labels[] = {"Output: ", "Repeat: ", "T0: ", "T1: ", "T2: ", "T3: ", "CV0: ", "CV1: ", "Scale: ", "Offset: ", "Sample Time: ", "Trig Crtl: ", "Trig Holdoff: "};
+String bounce1_labels[] = {"Output: ", "Repeat: ", "T0: ", "T1: ", "T2: ", "T3: ", "Scale: ", "Offset: ", "Sample Time: ", "Trig Crtl: ", "Trig Holdoff: "};
 Greenface_gadget bounce1_fxn("Bounce 1", bounce1_labels, bounce1_stuff, sizeof(_bounce1_params) / sizeof(_bounce1_params[0]));
 
-Greenface_gadget *bounce_inputs[NUM_OUTPUTS] = {
+int16_t intput_offsets0[] = {0, 0, 0, 0, 0, 0, OUTPUT_SCALE_OFFSET, OUTPUT_OFFSET_OFFSET, 0, 0, 0}; // allows negative numbers
+int16_t intput_offsets1[] = {0, 0, 0, 0, 0, 0, OUTPUT_SCALE_OFFSET, OUTPUT_OFFSET_OFFSET, 0, 0, 0}; // allows negative numbers
+
+int16_t *input_offsets[NUM_INPUTS] =
+    {
+        intput_offsets0,
+        intput_offsets1,
+};
+
+Greenface_gadget *bounce_inputs[NUM_INPUTS] = {
     &bounce0_fxn,
     &bounce1_fxn,
 };
@@ -52,7 +61,7 @@ int bounce_input()
 
 void bounce_init()
 {
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < NUM_INPUTS; i++)
     {
         (*bounce_inputs[i]).init();
     }
@@ -122,8 +131,6 @@ update_fxn bounce_update_fxns[BOUNCE_NUM_PARAMS] = {
     nullptr,
     nullptr,
     nullptr,
-    nullptr,
-    nullptr,
     bounce_update_trig_ctrl,
     bounce_update_trig_ctrl_vals,
 };
@@ -135,6 +142,7 @@ void bounce_begin()
         (*bounce_inputs[i]).begin();
         (*bounce_inputs[i]).string_params = bounce_string_params;
         (*bounce_inputs[i]).update_fxns = bounce_update_fxns;
+        (*bounce_inputs[i]).offsets = input_offsets[i];
     }
 
     // bounce0_fxn.begin();
@@ -155,16 +163,24 @@ String check_bounce()
     if (bounce_triggered && millis() > bounce_next_time)
     {
         // ui.terminal_debug("enabled: " + String(bounce_debug) + " trig_num: " + String(trig2.trig_num));
-        // ui.terminal_debug("Bounce Triggered by Trig: " + String(bounce_triggered_by) + " Reading: " + String(bounce_reading));
         // float reading = IN_FS_VOLTS * bounce_reading / 1650;
+        uint16_t the_bounce_input = bounce_input();
+        uint16_t output_num = (*bounce_inputs[the_bounce_input]).get_param(BOUNCE_OUTPUT);
+        float output_scale = float((*bounce_inputs[the_bounce_input]).get_param_w_offset(BOUNCE_SCALE)) / 100.0;
+        float output_offset = float((*bounce_inputs[the_bounce_input]).get_param_w_offset(BOUNCE_OFFSET)) * 19.65; // 20.48
+        int bounce_offset_correction = get_raw_output_offset_correction(output_num);
         float reading = scale_adc(bounce_reading);
+        // uint16_t dac_val = (bounce_reading * output_scale + 1660) * 1.234;
+        uint16_t dac_val = (bounce_reading * output_scale * 1.16) + 2048 + output_offset + bounce_offset_correction;
+        ui.terminal_debug(" Reading: " + String(bounce_reading) + " DAC val: " + String(dac_val) + " Offset: " + String(output_offset) + " corr: " + bounce_offset_correction);
+        dac_out(output_num, dac_val);
         bounce_triggered = selected_fxn->get_param(BOUNCE_REPEAT);
         bounce_next_time = millis() + selected_fxn->get_param(BOUNCE_SAMPLE_TIME);
         if (bounce_triggered)
         {
-            trigger_bounce(bounce_triggered_by[bounce_input()], TRIGGER_ACTIVE);
+            trigger_bounce(bounce_triggered_by[the_bounce_input], TRIGGER_ACTIVE);
         }
-        return "  CV" + String(bounce_input()) + " Reading: " + String(reading) + "    Triggered by: T" + String(bounce_triggered_by[bounce_input()]) + "  ";
+        return "  CV" + String(the_bounce_input) + " Reading: " + String(reading) + "    Triggered by: T" + String(bounce_triggered_by[the_bounce_input]) + "  ";
     }
     return "";
 }
