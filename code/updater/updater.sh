05129d42d514d3ec2773578e18715ea0f9aad76e
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-12-02 08:48:03 -0800

    Now user selects a number.

--- code/updater/updater.sh
+++ code/updater/updater.sh
@@ -1,13 +1,24 @@
 #!/bin/zsh
 
-# List available serial ports
+# List available serial ports and store them in an array
 echo "Available COM ports:"
-ls /dev/tty.*
+ports=(/dev/tty.*)
+for i in "${!ports[@]}"; do 
+    echo "$((i+1)): ${ports[$i]}"
+done
 
 while true; do
     # Ask the user to select a port
-    echo "Please enter the COM port you want to use: "
-    read selected_port
+    echo "Please enter the number of the COM port you want to use: "
+    read selected_port_number
+
+    # Check if the input is a number and if the number corresponds to a port
+    if [[ $selected_port_number =~ ^[0-9]+$ ]] && [ "$selected_port_number" -ge 1 ] && [ "$selected_port_number" -le ${#ports[@]} ]; then
+        selected_port=${ports[$((selected_port_number-1))]}
+    else
+        echo "Invalid selection. Please try again."
+        continue
+    fi
 
     # Confirm the user's selection
     echo "You selected $selected_port. Is this correct? (y/n) "
