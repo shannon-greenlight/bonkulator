e41333f143613a673e8250472f993fb06cfcf493
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-12-03 00:51:21 -0800

    Only bumps index on valid boards.

--- code/updater/updater.sh
+++ code/updater/updater.sh
@@ -16,13 +16,15 @@ index=1
 echo "$board_list" | while read -r line; do
     if [[ $line == *"/dev/cu"* ]]; then
         port=$(echo $line | awk '{print $1}')
-        ports+=("$port")
         if [[ $line == *"Arduino Nano RP2040 Connect"* ]]; then
             echo "$index: Bonkulator found on $port"
+            ports+=("$port")
+            ((index++))
         elif [[ $line == *"Arduino NANO 33 IoT"* ]]; then
             echo "$index: Spankulator found on $port"
+            ports+=("$port")
+            ((index++))
         fi
-        ((index++))
     fi
 done
 
