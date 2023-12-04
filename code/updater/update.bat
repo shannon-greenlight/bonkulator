5329ae2d1631f97c974d8c85d0ac63223a68ab30
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-12-04 09:03:20 -0800

    Fixes Mac upload
    User no longer needs to chmod and use sed to prepare release.

--- code/updater/update.bat
+++ code/updater/update.bat
@@ -6,7 +6,7 @@ type release.txt
 echo.
 
 REM Get a list of connected boards
-powershell -Command "$boardList = .\arduino-cli board list; $boardList -split \"`n\" | ForEach-Object { if ($_ -match '^(COM\d+)\s') { $port = $Matches[1]; if ($_ -match 'Arduino Nano RP2040 Connect') { Write-Host \"Bonkulator found on $port\" } else { Write-Host \"Spankulator found on $port\" } } }"
+powershell -Command "$boardList = .\arduino-cli board list; $boardList -split \"`n\" | ForEach-Object { if ($_ -match '^(COM\d+)\s') { $port = $Matches[1]; if ($_ -match 'Arduino Nano RP2040 Connect') { Write-Host \"Bonkulator found on $port\" } elseif ($_ -match 'Arduino NANO 33 IoT') { Write-Host \"Spankulator found on $port\" } } }"
 
 echo.
 set /p port=What's the COM port? (please enter a number) 'x' to exit 
