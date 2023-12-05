3cfc0213f6ef09c9ec569e50701c509c660e1713
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-12-05 02:39:09 -0800

    Update load_recent.sh

--- code/load_recent.sh
+++ code/load_recent.sh
@@ -41,15 +41,17 @@ while true; do
 
     if [ "$selected_port_number" = "" ]; then
         selected_port=$port
+        break
     else
         # Check if the input is a number and if the number corresponds to a port
         if [[ $selected_port_number =~ ^[0-9]+$ ]] && [ "$selected_port_number" -ge 1 ] && [ "$selected_port_number" -le ${#ports[@]} ]; then
             selected_port=${ports[$selected_port_number]}
+            break
         else
             echo "Invalid selection. Please try again."
             continue
         fi
     fi
-    echo $selected_port > spank_port.txt
 done
+echo $selected_port > spank_port.txt
 ./bin/arduino-cli upload -p $selected_port -b arduino:mbed_nano:nanorp2040connect -i ./out/Bonkulator.ino.bin
