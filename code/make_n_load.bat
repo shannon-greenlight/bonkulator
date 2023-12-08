d420cc7fd15691806bc5fc73ee9f51f10624d04a
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-12-08 05:56:41 -0800

    Times the make process and prints results in hh:mm:ss format

--- code/make_n_load.bat
+++ code/make_n_load.bat
@@ -1,4 +1,26 @@
-call ./make it
+@echo off
+setlocal
+
+:: Start the stopwatch
+for /f %%i in ('powershell -Command "(Get-Date).Ticks"') do set start=%%i
+
+:: Call the command
+call ./make
+
+:: Stop the stopwatch
+for /f %%i in ('powershell -Command "(Get-Date).Ticks"') do set end=%%i
+
+:: Calculate the elapsed time and convert it to HH:MM:SS format
+for /f "tokens=1-3 delims=:." %%a in ('powershell -Command "$elapsed = [timespan]::FromTicks(%end% - %start%); $elapsed.ToString('hh\:mm\:ss')"') do (
+   set hh=%%a
+   set mm=%%b
+   set ss=%%c
+)
+
+:: Print the elapsed time
+echo.
+echo Make took this much time: %hh%:%mm%:%ss%
+
 if errorlevel 1 (
    echo Compiler Error: %errorlevel%
    exit /b %errorlevel%
--- code/make_n_load.sh
+++ code/make_n_load.sh
@@ -1,8 +1,20 @@
 #!/bin/zsh
 
+start=$(date +%s)
+
 ./make.sh
 if [ $? -ne 0 ]; then
     echo "Compiler Error: $?"
     exit $?
 fi
+
+end=$(date +%s)
+
+elapsed=$(( end - start ))
+hours=$(( elapsed / 3600 ))
+minutes=$(( (elapsed % 3600) / 60 ))
+seconds=$(( elapsed % 60 ))
+
+printf 'Elapsed time: %02d:%02d:%02d\n' $hours $minutes $seconds
+
 ./load_recent.sh
