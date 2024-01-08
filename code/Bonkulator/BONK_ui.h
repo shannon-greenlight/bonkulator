d4e43bdfe702a1a727ac85f3f170119e5c671186
shannon-greenlight <shannon@greenlightdevelopment.com>
2024-01-08 12:05:31 -0800

    Fixes app crash
    Fixes crash in the app when you press a key to restore the display after it has entered screen-saver mode then send a command via the app.

--- code/Bonkulator/BONK_ui.h
+++ code/Bonkulator/BONK_ui.h
@@ -16,7 +16,7 @@ void terminal_print_status(String status_string, bool force = false)
   {
     int pos = TERMINAL_WIDTH / 2 - status_string.length() / 2;
     pos = pos + pos % 2;
-    if (ui.terminal_mirror & (status_string != old_status || force))
+    if (ui.terminal_mirror && (status_string != old_status || force))
     {
       // ui.t.setCursor(STATUS_ROW, "1");
       // ui.t.clrToEOL();
@@ -40,7 +40,7 @@ uint16_t get_chars_per_line()
 
 void restore_display()
 {
-  ui.all_on();
+  ui.all_on(!usb_direct_enabled());
   ui.reset_inactivity_timer();
   selected_fxn->display();
   terminal_print_status(old_status, true);
